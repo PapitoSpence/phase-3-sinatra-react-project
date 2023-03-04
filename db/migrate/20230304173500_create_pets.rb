87c47f5f6717da8bf117613ecbde50e850da8dd5
PapitoSpence <PapitoSpence>
2023-03-04 20:43:20 +0300

    Created the users and pets table and migrated

--- db/migrate/20230304173500_create_pets.rb
+++ db/migrate/20230304173500_create_pets.rb
@@ -1,4 +1,12 @@
 class CreatePets < ActiveRecord::Migration[6.1]
   def change
+    create_table :pets do |t|
+      t.string :name
+      t.string :description
+      t.string :breed
+      t.string :status
+      t.string :image
+      t.integer :user_id
+    end
   end
 end
